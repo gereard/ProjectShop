bebbf9e9c22b73704892abed44cb44ef711fb5b3
Guillem <gvestit@gmail.com>
2018-10-02 18:21:58 +0200

    Noves pàgines

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SComponent } from './s/s.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    ProfileComponent,
+    SComponent
   ],
   imports: [
     BrowserModule

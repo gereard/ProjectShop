263e567db689b93344fbf36cabe638426dc12a9f
Mateu <mateuserra@hotmail.com>
2018-10-18 16:47:01 +0200

    Login

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -8,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  signing: boolean;
   ngOnInit() {
+    this.signing = true;
   }
+  
+  
+  
+  changeLogin()
+    {
+        this.signing = true;
+        document.getElementById("tab-login").classList.add('active');
+        document.getElementById("tab-sign").classList.remove('active');
+        
+    }
+    
+    
+    changeSignIn(){
+            this.signing = false;
+            document.getElementById("tab-sign").classList.add('active');
+            document.getElementById("tab-login").classList.remove('active');
+           
 
 }
+
+
+}
+
+
